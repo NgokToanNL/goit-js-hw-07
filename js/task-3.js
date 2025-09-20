f1b8fefe661593e0db7fd6a956643786a13ec04f
NgokToanNL <96310301+NgokToanNL@users.noreply.github.com>
2025-09-20 19:13:04 +0200

    tasks 1-3

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+textInput.addEventListener("input", () => {
+    const trimmedInput = textInput.value.trim();
+
+    if (trimmedInput === "") {
+        output.textContent = "Anonymus";
+    }
+    else {
+        output.textContent = trimmedInput;
+    }
+} )
