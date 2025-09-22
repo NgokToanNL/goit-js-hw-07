fb15a0bd23c98006a11e46eefb41a7472ee61951
NgokToanNL <96310301+NgokToanNL@users.noreply.github.com>
2025-09-23 00:45:41 +0200

    task 4-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const backgroundColorBtn = document.querySelector("button");
+
+backgroundColorBtn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  
+  const nameofColor = document.querySelector("span");
+  nameofColor.textContent = randomColor;
+
+})
+
+
